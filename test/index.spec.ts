fa38f15d5a384e7c29735384330fc4b86cb64904
Naoki Ainoya <ainonic@gmail.com>
2025-12-25 17:21:47 +0900

    add ci

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -8,18 +8,20 @@ import worker from '../src/index';
 const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
 describe('Hello World worker', () => {
-	it('responds with Hello World! (unit style)', async () => {
+	it('responds with 401 (unit style)', async () => {
 		const request = new IncomingRequest('http://example.com');
 		// Create an empty context to pass to `worker.fetch()`.
 		const ctx = createExecutionContext();
 		const response = await worker.fetch(request, env, ctx);
 		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
 		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+		expect(response.status).toBe(401);
+		expect(await response.text()).toMatchInlineSnapshot(`"Authorization header is missing"`);
 	});
 
-	it('responds with Hello World! (integration style)', async () => {
+	it('responds with 401 (integration style)', async () => {
 		const response = await SELF.fetch('https://example.com');
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+		expect(response.status).toBe(401);
+		expect(await response.text()).toMatchInlineSnapshot(`"Authorization header is missing"`);
 	});
 });
